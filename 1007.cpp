ea3e6c15c01ee63159f718cd9d9e188f33f3e67f
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-06-26 17:20:00 -0300

    1007.cpp

---
+++ 1007.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b, c, d, dif;
+    cin >> a >> b >> c >> d;
+    dif = (a * b - c * d);
+    cout << "DIFERENCA = " << dif << endl;
+    return 0;
+}
+
+
