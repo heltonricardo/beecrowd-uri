5c7d935d0dc2a7f0a84ee3b82aa4ff50feb012b7
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-06-29 09:10:54 -0300

    1050.py

---
+++ 1050.py
@@ -0,0 +1,13 @@
+dados = {
+    61: 'Brasilia',
+    71: 'Salvador',
+    11: 'Sao Paulo',
+    21: 'Rio de Janeiro',
+    32: 'Juiz de Fora',
+    19: 'Campinas',
+    27: 'Vitoria',
+    31: 'Belo Horizonte'
+    }
+n = int(input())
+if n in dados: print(dados[n])
+else: print('DDD nao cadastrado')
