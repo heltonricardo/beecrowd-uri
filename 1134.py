08ad266f6f37fdf28f8422f7a717e2d4954893f0
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-07-03 09:46:58 -0300

    1134.py

---
+++ 1134.py
@@ -0,0 +1,11 @@
+a = g = d = 0
+c = 0
+while c != 4:
+    c = int(input())
+    if c == 1: a += 1
+    elif c == 2: g += 1
+    elif c == 3: d += 1
+print('MUITO OBRIGADO')
+print('Alcool:', a)
+print('Gasolina:', g)
+print('Diesel:', d)
