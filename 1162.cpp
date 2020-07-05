9e70214575350c21ce9043596ca99201ca2f6f04
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-07-05 11:14:12 -0300

    1162.cpp

---
+++ 1162.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+   short g, q, n, v[50], i, ind, t, x;
+
+   cin >> q;
+   for (g = 0; g < q; ++g) {
+      cin >> n;
+      for (i = 0; i < n; ++i)
+         cin >> v[i];
+      i = 1;
+      t = 0;
+      while (i <= n) {
+         while (v[i-1] != i) {
+            ind =
+         }
+      }
+   }
+
+   return 0;
+}
+
