5a860aebf1467b6c9526a9cd86e2142830707b45
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-07-07 09:16:54 -0300

    1188.py

---
+++ 1188.py
@@ -0,0 +1,10 @@
+op = str(input())
+s = q = 0
+for i in range(12):
+    for j in range(12):
+        n = float(input())
+        if i > j and i+j >= 12:
+            s += n
+            q += 1
+if op == 'S': print('{:.1f}'.format(s))
+else: print('{:.1f}'.format(s / q))
