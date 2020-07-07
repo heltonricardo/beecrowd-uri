73dee8d9d012c011faf9e61b787d5c1d9a4ef4c5
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-07-07 09:16:48 -0300

    1184.cpp

---
+++ 1184.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(void) {
+    double n, s = 0.0;
+    int i, j, q = 0;
+    char op;
+
+    cin >> op;
+    for (i = 0; i < 12; ++i)
+        for (j = 0; j < 12; ++j) {
+            cin >> n;
+            if (j < i) {
+                s += n;
+                ++q;
+            }
+    }
+    s = (op == 'S') ? s : double(s / q);
+    cout << fixed << setprecision(1);
+    cout << s << endl;
+    return 0;
+}
+
+
