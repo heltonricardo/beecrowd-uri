826577c4d49f206ebeca47ebfc995aba1fca4ea3
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-07-14 09:08:23 -0300

    1546.py

---
+++ 1546.py
@@ -0,0 +1,9 @@
+q = int(input())
+for p in range(q):
+    r = int(input())
+    for s in range(r):
+        n = int(input())
+        if n == 1: print('Rolien')
+        elif n == 2: print('Naej')
+        elif n == 3: print('Elehcim')
+        else: print('Odranoel')
