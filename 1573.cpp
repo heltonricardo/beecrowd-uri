5a80d0a130b92bb1a6bfda02444d6a65abb9dc83
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-07-15 09:08:54 -0300

    1573.cpp

---
+++ 1573.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+   int a, b, c;
+   while (cin >> a >> b >> c and (a or b or c))
+      cout << int(cbrt(a*b*c)) << endl;
+   return 0;
+}
+
