36e72a37e4a7b20ce6ab0c3420fb8e2a8eca3af7
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-07-15 09:08:52 -0300

    1564.cpp

---
+++ 1564.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    while(cin >> n)
+        if (n == 0) cout << "vai ter copa!" << endl;
+        else cout << "vai ter duas!" << endl;
+    return 0;
+}
+
