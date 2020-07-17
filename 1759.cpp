b370dc4df1047248c841541395613d6004938a37
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-07-17 09:38:55 -0300

    1759.cpp

---
+++ 1759.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+   int q, i;
+
+   cin >> q;
+
+   for (i = 0; i < q - 1; ++i)
+      cout << "Ho ";
+   cout << "Ho!" << endl;
+   return 0;
+}
