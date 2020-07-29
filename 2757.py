773e39a194203b7eedb400e9e3bcc6a68684b39f
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-07-29 09:25:47 -0300

    2757.py

---
+++ 2757.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+print('A = {}, B = {}, C = {}'.format(a, b, c))
+print('A = {:>10}, B = {:>10}, C = {:>10}'.format(a, b, c))
+print('A = {:010}, B = {:010}, C = {:010}'.format(a, b, c))
+print('A = {:<10}, B = {:<10}, C = {:<10}'.format(a, b, c))
